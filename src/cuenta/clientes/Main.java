9abc753e79ceab994872d898aa65229449ab1f40
wissp <wissp@DESKTOP-9U4ROO6>
2024-02-13 19:15:53 +0100

    first commit

--- src/cuenta/clientes/Main.java
+++ src/cuenta/clientes/Main.java
@@ -12,18 +12,18 @@ public class Main {
 
         cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
         
-        System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        System.out.println("El actual saldo es:"+ cuenta1.estado() );
 
         try {
             cuenta1.retirar(2300);
-            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+            System.out.println("El actual saldo es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
             cuenta1.ingresar(695);
-            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+            System.out.println("El actual saldo es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
